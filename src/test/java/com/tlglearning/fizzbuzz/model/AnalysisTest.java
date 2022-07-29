187e8d972ac38cf039fb364176ad67f9600542d0
Quentin M <mattson.quentin@gmail.com>
2022-07-29 12:40:12 -0700

    Turned Anonymous class into lambda.

--- src/test/java/com/tlglearning/fizzbuzz/model/AnalysisTest.java
+++ src/test/java/com/tlglearning/fizzbuzz/model/AnalysisTest.java
@@ -51,11 +51,6 @@ class AnalysisTest {
   @ParameterizedTest
   @ValueSource(ints = {-1, -3, -5, -15})
   void analyze_negative(int value) {
-    assertThrows(IllegalArgumentException.class, new Executable() {
-      @Override
-      public void execute() throws Throwable {
-        analysis.analyze(value);
-      }
-    });
+    assertThrows(IllegalArgumentException.class, () -> analysis.analyze(value));
   }
 }
